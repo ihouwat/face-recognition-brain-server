0d264a569fec7dd38ac19d78204257f296e86f57
ihouwat <houwat@gmail.com>
2020-02-08 14:53:30 -0500

    Moved Clarifai API from frontend

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,19 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+  apiKey: '23c15ac8a8294d77bc3df63381bd30ef'
+ });
+
+
+const handleApiCall = (req, res) => {
+  app.models
+    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
 const handleImage = (req, res, db) => {
   const { id } = req.body;
   db('users').where('id', '=', id)
@@ -10,5 +26,6 @@ const handleImage = (req, res, db) => {
 }
 
 module.exports = {
-  handleImage
+  handleImage,
+  handleApiCall
 }
